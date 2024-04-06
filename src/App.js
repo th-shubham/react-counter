a0d78934dc39361941ebae62ab64da6ce9588e26
Shubham Jangid <shubham0jangid@gmail.com>
2024-04-06 15:50:27 +0530

    Create Counter app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { Page } from "./StyleApp";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [step, setStep] = useState(1);
+	const [count, setCount] = useState(0);
+	const [date, updateDate] = useState(new Date());
+
+	const formatDate = (() => {
+		const formatter = new Intl.DateTimeFormat("en-US", {
+			weekday: "short",
+			year: "numeric",
+			month: "short",
+			day: "numeric",
+		});
+
+		return function (date) {
+			return formatter.format(new Date(date));
+		};
+	})();
+
+	const incrementStep = () => setStep((s) => s + 1);
+	const decrementStep = () => setStep((s) => s - 1);
+
+	const changeCountAndDate = (change) => {
+		const newCount = count + step * change;
+		const newDate = new Date();
+		newDate.setDate(newDate.getDate() + newCount);
+
+		setCount(newCount);
+		updateDate(newDate);
+	};
+	function dateTimeFormat() {
+		const today = new Date();
+		const diffTime = date - today;
+		const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+		const displayDate = formatDate(date);
+		if (diffDays === 0) {
+			return `Today is ${displayDate}`;
+		} else if (diffDays > 0) {
+			return `${diffDays} days from Today is ${displayDate}`;
+		} else {
+			return `${diffDays} days ago from Today was ${displayDate}`;
+		}
+	}
+	return (
+		<main style={Page}>
+			<div style={{ paddingBottom: "1rem" }}>
+				<button onClick={decrementStep}> - </button>
+				<span>{` Step : ${step} `}</span>
+				<button onClick={incrementStep}> + </button>
+			</div>
+			<div>
+				<button onClick={() => changeCountAndDate(-1)}> - </button>
+				<span>{` Count : ${count} `}</span>
+				<button onClick={() => changeCountAndDate(+1)}> + </button>
+			</div>
+
+			<h3>{dateTimeFormat()}</h3>
+		</main>
+	);
 }
 
 export default App;
---
+++ src/StyleApp.js
@@ -0,0 +1,9 @@
+export const Page = {
+	fontSize: "18px",
+	height: "50vh",
+	display: "flex",
+	justifyContent: "center",
+	alignItems: "center",
+	flexDirection: "column",
+	gap: "1rem",
+};
